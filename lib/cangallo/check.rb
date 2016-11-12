b7397f24d68258fb9e46febcee90cfc6e3244766
Javi Fontan <jfontan@gmail.com>
2016-11-12 20:12:59 +0100

    gh-20: check qemu-img version

--- lib/cangallo/check.rb
+++ lib/cangallo/check.rb
@@ -28,11 +28,12 @@ class Cangallo
       valid = valid_kernel = check_kernel
       help_kernel if !valid_kernel
 
+      valid = valid_qemu_img = check_qemu_img
+      help_qemu_img if !valid_qemu_img
 
       if !valid
-        text = "There is at least one one problem in your system."
+        text = "There is at least one problem in your system."
 
-        STDERR.puts
         STDERR.puts text
       end
 
@@ -54,6 +55,38 @@ class Cangallo
 libguestfs needs a kernel to boot it's qemu appliance. There is no kernel
 in your /boot directory readable by your kernel. Change the permissions of
 at least one kernel in that directory to be readable by the current user.
+
+      EOT
+
+      STDERR.puts text
+    end
+
+    def check_qemu_img
+      version = Cangallo::Qcow2.qemu_img_version
+
+      if !version
+        text = "Could not get qemu-img version. Is it installed in your system?"
+
+        STDERR.puts text
+        STDERR.puts
+        exit(-1)
+      end
+
+      good = Gem::Version.new('2.4.0')
+      current = Gem::Version.new(version)
+
+      current >= good
+    end
+
+    def help_qemu_img
+      text = <<-EOT
+Cangallo needs a qemu-img version equal or greater than 2.4.0. Yours appears
+to be older. This will make impossible to compress delta images. You can
+download this qemu-img binary and add it to the path. Make sure the path
+possition is before #{%x(which qemu-img).strip}.
+
+https://canga.io/downloads/qemu-img.bz2
+
       EOT
 
       STDERR.puts text
